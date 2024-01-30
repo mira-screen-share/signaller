eda08fc72ca445163111d130a0af672b7e84c37d
Mark Wang <mark.wang@databricks.com>
2024-01-30 16:16:46 -0500

    Allow fetching Twilio ice servers (#8)
    Need to deploy the config @harrynull

---
+++ src/args.rs
@@ -0,0 +1,9 @@
+use clap::Parser;
+
+#[derive(Parser, Debug, Clone)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    /// Listening Websocket address
+    #[arg(short, long, default_value = "0.0.0.0:8080")]
+    pub(crate) address: String,
+}
--- src/state.rs
+++ src/state.rs
@@ -1,10 +1,18 @@
-use crate::peer::{Peer, PeerType};
-use crate::session::Session;
+use std::collections::HashMap;
+use std::sync::Arc;
+
+use base64::Engine;
 use failure::{format_err, Error};
 use futures_channel::mpsc::UnboundedSender;
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use tokio::sync::Mutex;
 use tungstenite::protocol::Message;
+use twilio::TwilioAuthentication;
+
+use crate::config::Config;
+use crate::peer::{Peer, PeerType};
+use crate::session::Session;
+use crate::signaller_message::IceServer;
+use crate::twilio_helper::get_twilio_ice_servers;
 
 type Result<T> = std::result::Result<T, Error>;
 type Tx = UnboundedSender<Message>;
@@ -12,15 +20,37 @@ type Tx = UnboundedSender<Message>;
 pub struct State {
     pub sessions: HashMap<String, Session>,
     pub peers: HashMap<String, Peer>,
+    pub twilio_client: Option<twilio::TwilioClient>,
+    pub twilio_account_sid: Option<String>,
 }
 
 pub type StateType = Arc<Mutex<State>>;
 
 impl State {
-    pub fn new() -> StateType {
+    pub fn new(config: &Config) -> StateType {
+        let base64_engine = base64::engine::GeneralPurpose::new(
+            &base64::alphabet::STANDARD,
+            base64::engine::general_purpose::PAD,
+        );
         Arc::new(Mutex::new(State {
             sessions: Default::default(),
             peers: Default::default(),
+            twilio_client: {
+                if let (Some(account_sid), Some(auth_token)) =
+                    (&config.twilio_account_sid, &config.twilio_auth_token)
+                {
+                    Some(twilio::TwilioClient::new(
+                        "https://api.twilio.com",
+                        TwilioAuthentication::BasicAuth {
+                            basic_auth: base64_engine
+                                .encode(format!("{}:{}", account_sid, auth_token).as_bytes()),
+                        },
+                    ))
+                } else {
+                    None
+                }
+            },
+            twilio_account_sid: config.twilio_account_sid.clone(),
         }))
     }
 
@@ -91,5 +121,13 @@ impl State {
             .get(viewer_uuid)
             .ok_or_else(|| format_err!("Peer does not exist"))?;
         Ok(peer.room.clone())
+    }
+
+    pub async fn get_ice_servers(&self) -> Vec<IceServer> {
+        if let (Some(client), Some(sid)) = (&self.twilio_client, &self.twilio_account_sid) {
+            get_twilio_ice_servers(client, sid).await
+        } else {
+            vec![]
+        }
     }
 }
