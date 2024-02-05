cf9807be0853abfa3bae05b9e8ff3b3e03418cbb
Mark Wang <carbonylgp@gmail.com>
2024-02-05 17:09:06 -0500

    .

--- src/session.rs
+++ src/session.rs
@@ -1,16 +1,16 @@
 use std::collections::HashSet;
-use std::net::SocketAddr;
+use std::net::IpAddr;
 use std::time::SystemTime;
 
 pub struct Session {
     pub sharer: String,
     pub viewers: HashSet<String>,
     pub start_time: SystemTime,
-    pub sharer_ip: SocketAddr,
+    pub sharer_ip: IpAddr,
 }
 
 impl Session {
-    pub fn new(sharer: String, sharer_ip: SocketAddr) -> Self {
+    pub fn new(sharer: String, sharer_ip: IpAddr) -> Self {
         Session {
             sharer,
             viewers: Default::default(),
